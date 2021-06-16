89ab1749f41d7cde4fbb91b45c9620bfddbbb6d0
Ashwin Talwalkar <atalwalkar719@gmail.com>
2021-06-16 10:57:59 -0700

    simple changes

--- smart_mirror/src/App.js
+++ smart_mirror/src/App.js
@@ -1,6 +1,15 @@
-import MainPage from "./Components/MainPage"; 
 
-// this is the first component that will render and calls the MainPage Component
-const App = ()=> (<MainPage />);
+// see this is my home page and it goes to....
+import './index.css';
+import Clock from './Components/Clock/clock'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Clock/>
+    </div>
+  );
+}
 
 export default App;
