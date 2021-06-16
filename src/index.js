6e65099e1e5793aa7c860f102a0c5ae70c447c5f
Ashwin Talwalkar <atalwalkar719@gmail.com>
2021-06-15 19:00:48 -0700

    starting project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// This will tell react which component to start rendering and where in the html file to populate
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
