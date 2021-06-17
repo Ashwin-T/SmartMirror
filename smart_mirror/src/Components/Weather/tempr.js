3321161abd42cc7956fce2b53d23826ae89d71ff
Ashwin Talwalkar <atalwalkar719@gmail.com>
2021-06-17 14:20:47 -0700

    trying to connect to weather API and moving to local area

--- smart_mirror/src/Components/Weather/tempr.js
+++ smart_mirror/src/Components/Weather/tempr.js
@@ -1,23 +1,29 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios'; 
 
-
-
 const temp = async () =>{
     const path = 'https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=88d2efcaea28f04722ae5374cd196f84';
 
     const d = await axios.get(path);
     console.log(d.main);
-    return (75);
+
+    return (300);
 };
 
 const ShowTemp = () => {
     
+    const KtoF = (k) => {
+        const f = (((k-273.15)*1.8)+32);
+        
+        return(f);
+    }
+
+
     const [temperature, setTemperature] = useState(null);
 
     useEffect(() => {
         (async()=>{
-            setTemperature(await temp());
+            setTemperature(KtoF(await temp()));
         })();
 
     }, []);
